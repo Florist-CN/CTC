007cee973af5b88fa3e4b741d64107d9ed510449
Florist-CN <k520303@gmail.com>
2018-02-01 19:46:29 +0800

    add TokenLocker

--- contracts/CommunityCoin.sol
+++ contracts/CommunityCoin.sol
@@ -7,22 +7,26 @@ contract CommunityCoin is CappedToken, PausableToken {
 
   string public constant symbol = "CTC";
 
-  string public constant name = "CTC";
+  string public constant name = "Community Coin";
 
   uint8 public constant decimals = 18;
 
   uint public constant unit = 10 ** uint256(decimals);
-  uint public constant INITIAL_SUPPLY = 10 ** 2 * unit;
-  uint public constant lockTime = 120 days;
+
+  uint public constant tokenCap = 10 ** 2 * unit; //
+  
+  uint public constant lockPeriod = 120 days;
+  
   uint public startTime;
 
-  function CommunityCoin() public {
+  function CommunityCoin() CappedToken(tokenCap) public {
       totalSupply_ = 0;
       startTime = now;
+      pause();
     }
     
      function unpause() onlyOwner whenPaused public {
-    require(now > startTime + lockTime);
+    require(now > startTime + lockPeriod);
     super.unpause();
   }
 
--- contracts/TokenSale.sol
+++ contracts/TokenSale.sol
@@ -2,29 +2,44 @@ pragma solidity ^0.4.18;
 
 import '../node_modules/zeppelin-solidity/contracts/crowdsale/CappedCrowdsale.sol';
 import '../node_modules/zeppelin-solidity/contracts/crowdsale/FinalizableCrowdsale.sol';
+import '../node_modules/zeppelin-solidity/contracts/math/SafeMath.sol';
 
 import './CommunityCoin.sol';
 
 contract TokenSale is CappedCrowdsale, FinalizableCrowdsale {
 
-CommunityCoin public token;
-    uint constant private r = 8000 * 10 ** 27;
-    uint constant private c = 12000 * 10 ** 27;
-    address constant private w= 0x12c752AA48A18a7409C23363c2E85443f24F149A;
+    using SafeMath for uint;
+
+    
+    uint constant private _rate = 8000 ; //Token per ETH
+    uint constant private _cap  = 20 * 10 ** 18;//Tire 1 raise total 2000ETH
+    address constant private w= 0xca35b7d915458ef540ade6068dfe2f44e8fa733c;
+
+    event WalletChanged(address _wallet);
 
     function TokenSale()
-    Crowdsale(now, now + 30 minutes,r,w)
-    CappedCrowdsale(c) public{
-
+    Crowdsale(now, now + 30 minutes,_rate,w)
+    CappedCrowdsale(_cap) public {
     }
 
     function setToken(address tokenAddress) onlyOwner public {
         token = CommunityCoin(tokenAddress);
     }
 
-     function transferOwnership(address newOwner) public onlyOwner {
+     function transferToken(address newOwner) public onlyOwner {
     require(newOwner != address(0));
     token.transferOwnership(newOwner);
-    super.transferOwnership(newOwner);
   }
+    function setWallet(address _wallet) {
+        wallet = _wallet;
+        WalletChanged(_wallet);
+    }
+
+    function remainToken() view public returns(uint) {
+        return (cap - weiRaised).mul(_rate);
+    }
+
+    function finalization() internal {
+        transferToken(msg.sender);
+    }
 }
